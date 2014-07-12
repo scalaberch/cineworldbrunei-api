e8a008cd02ad4705f6a5ee76aaf31aa9f9f8121c
kiethmark bandiola <scalaberch69@gmail.com>
2014-07-13 02:08:12 +0800

    Added hasAlreadyVoted()

--- application/models/movievote.php
+++ application/models/movievote.php
@@ -26,7 +26,23 @@ class Movievote extends CI_Model{
     	}
     }
 
-    
+	/**
+	*	Method Name: hasAlreadyVoted()
+	*	Description: Checks if a user has already voted a certain movie
+	*	  
+	*	@param user: the id of the user that will vote on the movie
+	*	@param movie: the id of the movie that will be voted
+	*
+	*	@return boolean: true if user has already voted, otherwise false
+	**/
+
+	public function hasAlreadyVoted($movie, $user){
+		$sql = "SELECT idmovieVotes FROM movieVotes WHERE movie_idmovie=? AND users_idusers=?";
+		$query = $this->db->query($sql, array($movie, $user));
+		if ($query->num_rows() > 0){
+			return true;
+		} else { return false; }
+	}
 
 
 }
