5b562b9ebd977711ed09279e506be2c9a5b8ad99
kiethmark bandiola <scalaberch69@gmail.com>
2014-07-13 22:58:17 +0800

    Added registerUser()

--- application/models/user.php
+++ application/models/user.php
@@ -31,17 +31,36 @@ class User extends CI_Model {
 
 
 
-
     /**
     *   Method Name: register_user()
     *   Description: Registers a user to the database.
     *   Assumption: The user is not yet registered to the database
+    *               Also, all inputs are already in correct form such as email (<id>@<domain>.<tag>) and 
+    *                   contact number ( +<country_code><area/network_code><number> )
     *   
-    *   @param $userdata: An array that consists of the user's data grabbed from the social media
-    *       it used. Array is of format {}
+    *   @param ln:  User's Last Name
+    *   @param fn:  User's First Name
+    *   @param mn:  User's Middle Name
+    *   @param sai: User's social account id (from login action)
+    *   @param sah: User's social account handler (Facebook or Twitter)
+    *   @param a:   User's address
+    *   @param e:   User's email address
+    *   @param c:   User's contact number
+    *
+    *   @return
     **/
     
+    public function registerUser($ln, $fn, $mn, $sai, $sah, $a, $e, $c){
+        $data = array('user_last_name'=>$ln, 'user_first_name'=>$fn, 'user_middle_name'=>$mn,
+                        'social_acct_id'=>$sai, 'social_acct_handler'=>$sah, 'user_address'=>$a,
+                        'user_email'=>$e, 'user_contact_number'=>$c);
+        $sql = $this->db->insert_string('users', $data);
+        $query = $this->db->query($sql);
 
+        if ($this->db->insert_id() != null){
+            return true;
+        } else { return false; }
+    }
 
 
 }
